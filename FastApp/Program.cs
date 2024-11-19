85d8f6d9bd48f838921c83205881f49fb1acaea0
Nicolas Deslandes <ndeslandes@live.fr>
2024-11-19 22:02:46 +0000

    Switched powershell test script with AOT-published FastApp

---
+++ FastApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+if (args.Length == 0)
+{
+    Console.WriteLine("Please provide a script ID.");
+    return;
+}
+
+var scriptId = args[0];
+Console.WriteLine($"Starting script {scriptId}");
+
+var sw = Stopwatch.StartNew();
+Thread.Sleep(1000);
+Console.WriteLine($"Script {scriptId}: error output");
+Thread.Sleep(1000);
+Console.WriteLine($"Terminating script {scriptId}");
+Thread.Sleep(1000);
+Console.WriteLine($"Elapsed time: {sw.Elapsed.TotalSeconds:N2}s");
--- ProcessLauncher/Program.cs
+++ ProcessLauncher/Program.cs
@@ -1,23 +1,59 @@
 ﻿using System.Diagnostics;
-using System.Net.WebSockets;
+using System.Runtime.InteropServices;
 
-void Log(string message) => Console.WriteLine($"{DateTime.UtcNow:HH:mm:ss.ffff} - {message}");
+void Log(string message) => Console.WriteLine($"{DateTime.UtcNow:HH:mm:ss.ffffff} [{Thread.CurrentThread.ManagedThreadId,3}] - {message}");
 void LogThreadCount() => Log($"Thread count: {Process.GetCurrentProcess().Threads.Count}");
 string CleanupOutput(string output) => output.Trim().Replace("\r\n", " ").Replace("\n", " ");
 
+ThreadPool.SetMinThreads(200, 200);
+
+string GetExecutablePath()
+{
+    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+    {
+        return @"..\FastApp\bin\Release\net9.0\win-x64\publish\FastApp.exe";
+    }
+
+    if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+    {
+        return "../FastApp/bin/Release/net9.0/linux-x64/publish/FastApp";
+    }
+
+    throw new Exception("Unknown OS Platform");
+}
+
+var path = GetExecutablePath();
+if (!File.Exists(path))
+{
+    Console.Error.WriteLine("Couldn't find executable {0}", path);
+    Console.Error.WriteLine("Current path: {0}", Directory.GetCurrentDirectory());
+
+    Console.WriteLine("Will try to build it");
+    Process.Start(new ProcessStartInfo
+    {
+        FileName = "dotnet",
+        Arguments = "publish -c Release",
+        WorkingDirectory = "../FastApp",
+    })!.WaitForExit();
+
+    if (!File.Exists(path))
+        return 1;
+}
+
 var sw = Stopwatch.StartNew();
 LogThreadCount();
-var tasks = Enumerable.Range(1, 25).Select(RunProcess);
+var tasks = Enumerable.Range(1, 40).Select(RunProcess);
 
 LogThreadCount();
 Task.WaitAll(tasks);
 
 LogThreadCount();
 Log($"All done in {sw.Elapsed.TotalSeconds:N2}s!");
+return 0;
 
 Task RunProcess(int id)
 {
-    var startInfo = new ProcessStartInfo(@"pwsh.exe", $"Script.ps1 {id}")
+    var startInfo = new ProcessStartInfo(path, id.ToString())
     {
         RedirectStandardOutput = true,
         RedirectStandardError = true,

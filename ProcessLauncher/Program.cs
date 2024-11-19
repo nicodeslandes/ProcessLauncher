3ed6503d3b458655e808d052997dd30490d8db8a
Nicolas Deslandes <ndeslandes@live.fr>
2024-11-19 20:43:34 +0000

    Create multiple child processes

--- ProcessLauncher/Program.cs
+++ ProcessLauncher/Program.cs
@@ -1,22 +1,34 @@
 ﻿using System.Diagnostics;
 
-var id = 42;
-var startInfo = new ProcessStartInfo(@"pwsh.exe", $"Script.ps1 {id}")
-{
-    RedirectStandardOutput = true,
-    RedirectStandardError = true,
-};
-
 void PrintThreadCount() => Console.WriteLine("Thread count: {0}", Process.GetCurrentProcess().Threads.Count);
 
 PrintThreadCount();
-var process = new Process { StartInfo = startInfo };
+var tasks = Enumerable.Range(1, 25).Select(RunProcess);
 
-Console.WriteLine("Starting process");
 PrintThreadCount();
-process.Start();
-Console.WriteLine("Process started");
+Task.WaitAll(tasks);
+
+Console.WriteLine("All done!");
 PrintThreadCount();
-var output = process.StandardOutput.ReadToEnd();
-Console.WriteLine("Process output: {0}", output);
-PrintThreadCount();
+
+Task RunProcess(int id)
+{
+    var startInfo = new ProcessStartInfo(@"pwsh.exe", $"Script.ps1 {id}")
+    {
+        RedirectStandardOutput = true,
+        RedirectStandardError = true,
+    };
+
+    return Task.Run(() =>
+    {
+        var process = new Process { StartInfo = startInfo };
+        Console.WriteLine("Starting process {0}", id);
+        process.Start();
+        Console.WriteLine("Process {0} started", id);
+        PrintThreadCount();
+        var output = process.StandardOutput.ReadToEnd();
+        Console.WriteLine("Process {0} output: {1}", id, output);
+        var error = process.StandardError.ReadToEnd();
+        Console.WriteLine("Process {0} error: {1}", id, error);
+    });
+}
--- ProcessLauncher/Script.ps1
+++ ProcessLauncher/Script.ps1
@@ -2,9 +2,9 @@ $ErrorActionPreference = 'Continue'
 
 $scriptId = $args[0]
 Write-Output "Starting script $scriptId"
-Start-Sleep -Seconds 3
+Start-Sleep -Seconds 1
 Write-Error "Script ${scriptId}: error output"
-Start-Sleep -Seconds 3
+Start-Sleep -Seconds 1
 Write-Output "Terminating script $scriptId"
 Start-Sleep -Seconds 1
 

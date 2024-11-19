be7dc2b76a473aa39b35d6ba0aa31ac3da67a550
Nicolas Deslandes <ndeslandes@live.fr>
2024-11-19 23:49:13 +0000

    Remove lambdas to ease debuggin

--- ProcessLauncher/Program.cs
+++ ProcessLauncher/Program.cs
@@ -1,117 +1,136 @@
 ﻿using System.Diagnostics;
+using System.IO;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Channels;
 
-const int Parallelism = 50;
-//ThreadPool.SetMaxThreads(5, 5);
-
-void Log(string message) => Console.WriteLine($"{DateTime.UtcNow:HH:mm:ss.ffffff} [{Thread.CurrentThread.ManagedThreadId,3}] - {message}");
-void LogThreadCount() => Log($"Thread count: {Process.GetCurrentProcess().Threads.Count}");
-string CleanupOutput(string output) => output.Trim().Replace("\r\n", " ").Replace("\n", " ");
-
-string GetExecutablePath()
+public class Program
 {
-    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+    private static void Log(string message) => Console.WriteLine($"{DateTime.UtcNow:HH:mm:ss.ffffff} [{Thread.CurrentThread.ManagedThreadId,3}] - {message}");
+    private static void LogThreadCount() => Log($"Thread count: {Process.GetCurrentProcess().Threads.Count}");
+    private static string CleanupOutput(string output) => output.Trim().Replace("\r\n", " ").Replace("\n", " ");
+
+    private static async Task<int> Main()
     {
-        return @"..\FastApp\bin\Release\net9.0\win-x64\publish\FastApp.exe";
+        const int Parallelism = 50;
+        //ThreadPool.SetMaxThreads(5, 5);
+
+        var path = LocateOrBuildFastApp();
+
+        var sw = Stopwatch.StartNew();
+        LogThreadCount();
+
+        var outputChannel = Channel.CreateUnbounded<(int id, string message)>();
+
+        var writerTask = StartOutputWriter(outputChannel.Reader);
+        var tasks = Enumerable.Range(1, Parallelism).Select(i => RunProcess(i, outputChannel.Writer, path));
+
+        LogThreadCount();
+        await Task.WhenAll(tasks);
+        outputChannel.Writer.Complete();
+
+        await writerTask;
+
+        LogThreadCount();
+        Log($"All done in {sw.Elapsed.TotalSeconds:N2}s!");
+        return 0;
     }
 
-    if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+    private static Task StartOutputWriter(ChannelReader<(int id, string message)> reader)
     {
-        return "../FastApp/bin/Release/net9.0/linux-x64/publish/FastApp";
-    }
-
-    throw new Exception("Unknown OS Platform");
-}
-
-var path = GetExecutablePath();
-if (!File.Exists(path))
-{
-    Console.Error.WriteLine("Couldn't find executable {0}", path);
-    Console.Error.WriteLine("Current path: {0}", Directory.GetCurrentDirectory());
-
-    Console.WriteLine("Will try to build it");
-    Process.Start(new ProcessStartInfo
-    {
-        FileName = "dotnet",
-        Arguments = "publish -c Release",
-        WorkingDirectory = "../FastApp",
-    })!.WaitForExit();
-
-    if (!File.Exists(path))
-        return 1;
-}
-
-var sw = Stopwatch.StartNew();
-LogThreadCount();
-
-var outputChannel = Channel.CreateUnbounded<(int id, string message)>();
-
-var writerTask = StartOutputWriter(outputChannel.Reader);
-var tasks = Enumerable.Range(1, Parallelism).Select(i => RunProcess(i, outputChannel.Writer));
-
-LogThreadCount();
-await Task.WhenAll(tasks);
-outputChannel.Writer.Complete();
-
-await writerTask;
-
-LogThreadCount();
-Log($"All done in {sw.Elapsed.TotalSeconds:N2}s!");
-return 0;
-
-Task RunProcess(int id, ChannelWriter<(int id, string message)> writer)
-{
-    var startInfo = new ProcessStartInfo(path, id.ToString())
-    {
-        RedirectStandardOutput = true,
-        RedirectStandardError = true,
-    };
-
-    return Task.Factory.StartNew(() =>
-    {
-        var process = new Process { StartInfo = startInfo };
-
-        Log($"Starting process {id}");
-        process.Start();
-        Log($"Process {id} started");
-
-        var outputBuilder = new StringBuilder(16384);
-
-        async void HandleStreamOutput(object sender, DataReceivedEventArgs e)
+        return Task.Run(async () =>
         {
-            lock (outputBuilder)
+            await foreach (var (id, message) in reader.ReadAllAsync())
             {
-                outputBuilder.AppendLine(e.Data);
+                Log($"From process {id,3}: {message}");
+            }
+        });
+    }
+
+    private static Task RunProcess(int id, ChannelWriter<(int id, string message)> writer, string path)
+    {
+        var runner = new ProcessRunner(id, writer, path);
+        return Task.Factory.StartNew(runner.Run, TaskCreationOptions.LongRunning);
+    }
+
+    class ProcessRunner(int id, ChannelWriter<(int id, string message)> writer, string path)
+    {
+        private readonly StringBuilder _outputBuilder = new(16384);
+
+        public void Run()
+        {
+            var startInfo = new ProcessStartInfo(path, id.ToString())
+            {
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+            };
+
+            var process = new Process { StartInfo = startInfo };
+
+            Log($"Starting process {id}");
+            process.Start();
+            Log($"Process {id} started");
+
+            process.OutputDataReceived += HandleStreamOutput;
+            process.ErrorDataReceived += HandleStreamOutput;
+
+            LogThreadCount();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+
+            Log($"Process {id} BeginOutput/ErrorReadLine called");
+
+            LogThreadCount();
+            process.WaitForExit();
+
+            Log($"Process {id} output: {CleanupOutput(_outputBuilder.ToString())}");
+        }
+
+        private async void HandleStreamOutput(object sender, DataReceivedEventArgs e)
+        {
+            lock (_outputBuilder)
+            {
+                _outputBuilder.AppendLine(e.Data);
             }
             await writer.WriteAsync((id, e.Data ?? ""));
 
         }
+    }
 
-        process.OutputDataReceived += HandleStreamOutput;
-        process.ErrorDataReceived += HandleStreamOutput;
-
-        LogThreadCount();
-        process.BeginOutputReadLine();
-        process.BeginErrorReadLine();
-
-        Log($"Process {id} BeginOutput/ErrorReadLine called");
-
-        LogThreadCount();
-        process.WaitForExit();
-
-        Log($"Process {id} output: {CleanupOutput(outputBuilder.ToString())}");
-    }, TaskCreationOptions.LongRunning);
-}
-
-Task StartOutputWriter(ChannelReader<(int id, string message)> reader)
-{
-    return Task.Run(async () =>
+    private static string LocateOrBuildFastApp()
     {
-        await foreach (var (id, message) in reader.ReadAllAsync())
+        var path = GetExecutablePath();
+        if (File.Exists(path))
+            return path;
+        Console.Error.WriteLine("Couldn't find executable {0}", path);
+        Console.Error.WriteLine("Current path: {0}", Directory.GetCurrentDirectory());
+
+        Console.WriteLine("Will try to build it");
+        Process.Start(new ProcessStartInfo
         {
-            Log($"From process {id, 3}: {message}");
+            FileName = "dotnet",
+            Arguments = "publish -c Release",
+            WorkingDirectory = "../FastApp",
+        })!.WaitForExit();
+
+        if (!File.Exists(path))
+            throw new Exception("Unable to find or build Fast App");
+
+        return path;
+    }
+
+    private static string GetExecutablePath()
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            return @"..\FastApp\bin\Release\net9.0\win-x64\publish\FastApp.exe";
         }
-    });
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            return "../FastApp/bin/Release/net9.0/linux-x64/publish/FastApp";
+        }
+
+        throw new Exception("Unknown OS Platform");
+    }
 }
